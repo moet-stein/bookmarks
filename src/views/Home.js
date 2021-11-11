c38c9401ed0f8498df91463e4a33e88aa43ab332
Moe Steinmueller <my email address used on my git repo>
2021-11-11 17:52:26 +0100

    changed email

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 const Home = () => {
   return (
     <Typography align="center" variant="h1">
-      BookmarK HOME PAGE
+      BOOKMARK HOME PAGE
     </Typography>
   );
 };
