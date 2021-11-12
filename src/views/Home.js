ab9b341d7f145ffe1e0b79af03c86a2ebe066ebd
Moe Steinmueller <0101.mste@gmail.com>
2021-11-12 15:49:04 +0100

    added dropdown btn

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,25 @@
 import React from "react";
 import AddGroup from "../components/AddGroup";
-import { Typography } from "@mui/material";
+import { Typography, Button, IconButton } from "@mui/material";
 import classes from "./styles/Home.module.css";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
 
 const Home = () => {
   return (
     <React.Fragment>
       <div className={classes.topPart}>
-        <Typography align="center" variant="h3">
-          Moe's Bookmarks
-        </Typography>
+        <div className={classes.flex}>
+          <Typography align="center" variant="h3">
+            Moe's Bookmarks
+          </Typography>
+          <IconButton
+            onClick={() => console.log("hello")}
+            variant="outlined"
+            className={classes.buttonMargin}
+          >
+            <ArrowDropDownCircleIcon />
+          </IconButton>
+        </div>
         <div className={classes.buttons}>
           <AddGroup />
         </div>
